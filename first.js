e10671f8dc88177338f77ec65a06dc88273956a2
Oluwadamilare Yusuf <damilareyusuf.dy@gmail.com>
2022-06-24 08:14:16 -0700

    My first Javascript lines of code
    Just Printed something on the screen using the alert output

---
+++ first.js
@@ -0,0 +1,11 @@
+function details() {
+        var myName = "Oluwadamilare Yusuf";
+        var height = 176;
+        var country = "Nigeria."
+        
+        alert('My name is ' + myName + ', i am' + height + 
+                'cm tall and i come from ' + country);  
+}
+
+
+
